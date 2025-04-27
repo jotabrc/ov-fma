2059f68920c0be1af0f5c3978d093828f305337f
jotabrc <ojcroveda@gmail.com>
2025-04-27 03:25:46 -0300

    feat: routing config and env loading

--- ov-fma-gateway/src/main/java/io/github/jotabrc/ov_fma_gateway/config/RouteConfig.java
+++ ov-fma-gateway/src/main/java/io/github/jotabrc/ov_fma_gateway/config/RouteConfig.java
@@ -8,6 +8,8 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class RouteConfig implements PropertiesWhitelistLoader {
@@ -24,9 +26,29 @@ public class RouteConfig implements PropertiesWhitelistLoader {
         try (InputStream stream = getClass().getClassLoader().getResourceAsStream(path)) {
             Properties properties = new Properties();
             properties.load(stream);
-            properties.forEach((k,v) -> routeConfig.put(k.toString(), v.toString()));
+            properties.forEach((k,v) -> {
+                String resolvedValue = resolveEnVar(v.toString());
+                routeConfig.put(k.toString(), resolvedValue);
+            });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String resolveEnVar(String value) {
+        Pattern pattern = Pattern.compile("\\$\\{([^}]+)}");
+        Matcher matcher = pattern.matcher(value);
+        StringBuilder resolvedValue = new StringBuilder();
+
+        String env = null;
+        while (matcher.find()) {
+            if (matcher.group(1).equals("segment")) continue;
+            env = System.getenv(matcher.group(1));
+            if (env == null || env.trim().isEmpty())
+                env = "";
+            matcher.appendReplacement(resolvedValue, env);
+        }
+        matcher.appendTail(resolvedValue);
+        return resolvedValue.toString();
     }
 }
