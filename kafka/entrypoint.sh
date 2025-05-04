608cb75ac9e07459aca461526f50b2209a6fef7d
jotabrc <ojcroveda@gmail.com>
2025-05-04 19:16:20 -0300

    feat: Kafka replicas configuration Three brokers

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -13,9 +13,5 @@ wait_for_port() {
 }
 
 wait_for_port "zookeeper" 2181
-
-#BROKER_ID=$(echo "$HOSTNAME" | grep -o '[0-9]*' | head -1)
-#export KAFKA_BROKER_ID=${BROKER_ID:-1}
-#export BROKER_HOST=$(hostname -f)
-#export KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://${BROKER_HOST}:9092"
+sleep 60
 exec /etc/confluent/docker/run
